d2e5ee0fd720405f2e232169a68f5c5f487f12d5
guodehua <18101029391@163.com>
2026-05-24 22:15:24 +0800

    修改飞书文档bot为单一bot

--- app/settings/config.py
+++ app/settings/config.py
@@ -65,6 +65,9 @@ class Settings(BaseSettings):
     FEISHU_BASE_URL: str = "https://open.feishu.cn/open-apis"
     FEISHU_STRUCTURED_FOLDER_TOKEN: str = ""
     FEISHU_VERIFY_SSL: bool = True
+    FEISHU_DOC_BOT_APPID: str = ""
+    FEISHU_DOC_BOT_APPSECRET: str = ""
+    
 
     # Gotenberg 文档转换服务
     GOTENBERG_URL: str = "http://localhost:3001"
